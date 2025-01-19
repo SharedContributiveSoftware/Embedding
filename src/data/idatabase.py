575e41b5af1b73fcbae90e7b0bbe8f4e982c3327
lugarte <lugarte>
2025-01-19 21:57:02 +0000

    change: correct abstract methods naming

--- src/data/idatabase.py
+++ src/data/idatabase.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class IDatabase(ABC):
     database_name: str = None
     connection_name: str = None
@@ -9,16 +10,22 @@ class IDatabase(ABC):
     def connect(cls, name: str):
         raise NotImplementedError("Not implemented yet")
 
+    @classmethod
     @abstractmethod
-    def disconnect(self):
+    def disconnect(cls):
         raise NotImplementedError("Not implemented yet")
 
-    def get_connection_name(self):
-        return self.connection_name
-
-    def get_database_name(self):
-        return self.database_name
-
+    @classmethod
     @abstractmethod
-    def get_cursor(self):
+    def get_connection_name(cls):
+        return cls.connection_name
+
+    @classmethod
+    @abstractmethod
+    def get_database_name(cls):
+        return cls.database_name
+
+    @classmethod
+    @abstractmethod
+    def get_cursor(cls):
         raise NotImplementedError("Not implemented yet")
