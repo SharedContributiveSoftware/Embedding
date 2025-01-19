8c54d677264871a9a01c150eee746dee0a50fc06
lugarte <lugarte>
2025-01-19 19:34:17 +0000

    change: new BASE_URL in constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from typing import Final
 
+BASE_URL: Final[str] = "https://store.steampowered.com/appreviews/50"
 SAMPLES_PATH: Final[Path] = Path("./samples")
 EMBEDDING_SAMPLE_PATH = SAMPLES_PATH / "mock_embedding.json"
 with open(EMBEDDING_SAMPLE_PATH, "r") as fd:
